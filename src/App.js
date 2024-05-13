af0ba8d2e730b3f487dc037f513aa1f5302aae3b
nelfen <nelfen.dev@gmail.com>
2024-05-13 20:17:36 +0900

    k

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import { useEffect, useState } from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("I'm here!")
-    return <h1>Hello</h1>
-  }, []);
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-  return (
+  const [toDo, setTodo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) =>[toDo, ...currentArray]);
+    setTodo("");
+  };
+  console.log(toDos);
+  return ( 
   <div>
-    {showing ? <Hello /> : null}
-    <button onClick={onClick}>{showing ? "hide" : "Show"}</button>
+    <h1>My To Dos ({toDos.length})</h1>
+    <form onSubmit={onSubmit}>
+      <input onChange={onChange} value={toDo} type="text" placeholder="write your to do..."/>
+    <button>Add To Do 726</button>
+    </form>
+    <hr />
+    <ul>
+      {toDos.map((item) => <li>{item}</li>)}
+    </ul>
   </div>
   );
 }
