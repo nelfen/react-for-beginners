0dd2efc51db90b26e6fb8b327f41933d32e2b8bd
nelfen <nelfen.dev@gmail.com>
2024-05-16 10:46:38 +0900

    7.5 - React Router

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,12 @@
 import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
 
 function Movie({coverImg, title, summary, genres}) {
   return <div>
   <img src={coverImg} alt={title}/>
-  <h2>{title}</h2>
+  <h2>
+    <Link to="/movie/:id">{title}</Link>
+  </h2>
   <p>{summary}</p>
   <ul>
     {genres.map((g) => (
@@ -19,4 +22,5 @@ Movie.prototypes = {
   summary: PropTypes.string.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 }
+
 export default Movie;
